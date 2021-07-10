e8306356aeae4392619245b28cb1704e2643c593
dsandy12 <dlsandy@asu.edu>
2021-07-09 21:07:28 -0700

    added tabs to main view with the ability to switch between tab views.  Added sensor create/modify tab controls

---
+++ configurator/src/main/java/org/picmg/configurator/SensorsTabController.java
@@ -0,0 +1,28 @@
+package org.picmg.configurator;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
+import org.picmg.jsonreader.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class SensorsTabController implements Initializable {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+}
