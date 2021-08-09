853311e9440fa0298ddced8ac097a59e4cd650c5
David Sandy <66977682+dasandy@users.noreply.github.com>
2021-08-08 22:31:36 -0700

    Added Copyright Information

--- configurator/src/main/java/org/picmg/configurator/App.java
+++ configurator/src/main/java/org/picmg/configurator/App.java
@@ -1,3 +1,25 @@
+//*******************************************************************
+//    App.java
+//
+//    More information on the PICMG IoT data model can be found within
+//    the PICMG family of IoT specifications.  For more information,
+//    please visit the PICMG web site (www.picmg.org)
+//
+//    Copyright (C) 2020,  PICMG
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 package org.picmg.configurator;
 
 import java.io.IOException;
