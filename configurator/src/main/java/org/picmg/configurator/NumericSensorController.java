0dd2d1aa7785ac4dff97c0d78b5666918d172cb3
David Sandy <66977682+dasandy@users.noreply.github.com>
2021-08-17 10:25:46 -0700

    repaired state set search, numeric sensor population

--- configurator/src/main/java/org/picmg/configurator/NumericSensorController.java
+++ configurator/src/main/java/org/picmg/configurator/NumericSensorController.java
@@ -21,22 +21,39 @@
 //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 package org.picmg.configurator;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.picmg.jsonreader.JsonAbstractValue;
 import org.picmg.jsonreader.JsonArray;
 import org.picmg.jsonreader.JsonObject;
+import org.picmg.jsonreader.JsonResultFactory;
 
 public class NumericSensorController implements Initializable {
 	@FXML private AnchorPane numericSensorPane;
@@ -46,10 +63,11 @@ public class NumericSensorController implements Initializable {
 	@FXML private ImageView boundChannelIcon;
 	@FXML private ComboBox<String> boundChannel;
 	@FXML private ImageView physicalSensorIcon;
-	@FXML private TextField physicalSensor;
+	@FXML private ComboBox<String> physicalSensor;
 	@FXML private ImageView inputCurveIcon;
 	@FXML private CheckBox inputCurveEnabled;
 	@FXML private Button selectCurve;
+	@FXML private Button view;
 	@FXML private ImageView inputGearingRatioIcon;
 	@FXML private TextField inputGearingRatio;
 	@FXML private ImageView physicalBaseUnitIcon;
@@ -59,7 +77,7 @@ public class NumericSensorController implements Initializable {
 	@FXML private ImageView physicalRateIcon;
 	@FXML private ComboBox<String> physicalRate;
 	@FXML private ImageView relIcon;
-	@FXML private TextField rel;
+	@FXML private ComboBox rel;
 	@FXML private ImageView physicalAuxIcon;
 	@FXML private ComboBox<String> physicalAux;
 	@FXML private ImageView physicalAuxRateIcon;
@@ -71,14 +89,162 @@ public class NumericSensorController implements Initializable {
 	private TreeItem<MainScreenController.TreeData> selectedNode;
 	private boolean updated = false;
 
+	public class NumericSensorData{
+		public String boundChannel;
+		public String physicalSensor;
+		public File inputCurve;
+		public String inputGearingRatio;
+		public String baseUnit;
+		public String unitModifier;
+		public String rateUnit;
+		public String auxUnit;
+		public String auxRateRelationship;
+		public String auxRate;
+		public String auxModifier;
+		ArrayList<String> sensorNames = new ArrayList<String>();
+		ArrayList<Point2D> inputCurvePoints = new ArrayList<>();
+	}
+
+	// choice box choices
+	final String[] unitsChoices = {
+			"None","Unspecified","Degrees_C", "Degrees_F", "Kelvins", "Volts", "Amps", "Watts", "Joules", "Coulombs",
+			"VA", "Nits", "Lumens", "Lux", "Candelas", "kPa", "PSI", "Newtons", "CFM", "RPM", "Hertz",
+			"Seconds", "Minutes", "Hours", "Days", "Weeks", "Mils", "Inches", "Feet", "Cubic_Inches",
+			"Cubic_Feet", "Meters", "Cubic_Centimeters", "Cubic_Meters", "Liters", "Fluid_Ounces",
+			"Radians", "Steradians", "Revolutions", "Cycles", "Gravities", "Ounces", "Pounds",
+			"Foot-Pounds", "Ounce-Inches", "Gauss", "Gilberts", "Henries", "Farads", "Ohms", "Siemens",
+			"Moles", "Becquerels", "PPM+(parts/million)", "Decibels", "DbA", "DbC", "Grays", "Sieverts",
+			"Color_Temperature_Degrees_K", "Bits", "Bytes", "Words_(data)", "DoubleWords", "QuadWords",
+			"Percentage", "Pascals", "Counts", "Grams", "Newton-meters", "Hits", "Misses", "Retries",
+			"Overruns/Overflows", "Underruns", "Collisions", "Packets", "Messages", "Characters",
+			"Errors", "Corrected_Errors", "Uncorrectable_Errors", "Square_Mils", "Square_Inches",
+			"Square_Feet", "Square_Centimeters", "Square_Meters"
+	};
+	final String[] rateChoices = {
+			"None","Per_MicroSecond","Per_MilliSecond","Per_Second","Per_Minute","Per_Hour",
+			"Per_Day","Per_Week","Per_Month","Per_Year"
+	};
+	final String[] relChoices = {
+			"dividedBy","multipliedBy"
+	};
+
+	/**
+	 * loadPointsFromCsvFile()
+	 * load points from the specified csv file into the data array for this object
+	 * @param input - the file to input from
+	 * @return - true if successful, otherwise false.
+	 */
+	public boolean loadPointsFromCsvFile(File input) {
+		// clear any existing points
+		data.inputCurvePoints.clear();
+
+		// attempt to load the new points
+		try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+			String csvLine;
+			// keep reading lines from the file until there are no more
+			while ((csvLine = br.readLine()) != null) {
+				String[] vals = csvLine.split(",");
+				// every line must have two values
+				if (vals.length!=2) return false;
+
+				// both values must be numeric
+				if (!App.isFloat(vals[0])) return false;
+				if (!App.isFloat(vals[1])) return false;
+
+				// this point checks out - make a point object and
+				// add it to the response curve.
+				Point2D p = new Point2D(Double.parseDouble(vals[0]),Double.parseDouble(vals[1]));
+				data.inputCurvePoints.add(p);
+			}
+		} catch (FileNotFoundException e) {
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+		// valid curves must have at least two points
+		if (data.inputCurvePoints.size()<2) return false;
+		return true;
+	}
+
+
+	private NumericSensorData data = new NumericSensorData();
+
+	private void updateIcons(){
+		//update icons
+		if (boundChannel.getValue() == null) {
+			boundChannelIcon.setVisible(true);
+		}else {
+			boundChannelIcon.setVisible(false);
+		}
+		if (physicalSensor.getValue() == null) {
+			physicalSensorIcon.setVisible(true);
+		}else {
+			physicalSensorIcon.setVisible(false);
+		}
+		if(inputCurveEnabled.isSelected()){
+			if(data.inputCurve==null){
+				inputCurveIcon.setVisible(true);
+			}else{
+				inputCurveIcon.setVisible(false);
+			}
+		}else{
+			inputCurveIcon.setVisible(false);
+		}
+		if (data.inputGearingRatio == null) {
+			inputGearingRatioIcon.setVisible(true);
+		}else {
+			inputGearingRatioIcon.setVisible(false);
+		}
+		if (physicalBaseUnit.getValue() == null) {
+			physicalBaseUnitIcon.setVisible(true);
+		}else {
+			physicalBaseUnitIcon.setVisible(false);
+		}
+		if (data.unitModifier == null) {
+			physicalUnitModifierIcon.setVisible(true);
+		}else {
+			physicalUnitModifierIcon.setVisible(false);
+		}
+		if (physicalRate.getValue() == null) {
+			physicalRateIcon.setVisible(true);
+		}else {
+			physicalRateIcon.setVisible(false);
+		}
+		if (rel.getValue() == null) {
+			relIcon.setVisible(true);
+		}else {
+			relIcon.setVisible(false);
+		}
+		if (physicalAux.getValue() == null) {
+			physicalAuxIcon.setVisible(true);
+		}else {
+			physicalAuxIcon.setVisible(false);
+		}
+		if (physicalAuxRate.getValue() == null) {
+			physicalAuxRateIcon.setVisible(true);
+		}else {
+			physicalAuxRateIcon.setVisible(false);
+		}
+		if (data.auxModifier == null) {
+			physicalAuxRateModifierIcon.setVisible(true);
+		}else {
+			physicalAuxRateModifierIcon.setVisible(false);
+		}
+	}
+
 	public boolean isError(){
 		if(updated) {
 			boolean isError = false;
 			if (boundChannel.getValue() == null) {
 				isError = true;
 			}
-			if (physicalSensor.getText() == null) {
+			if (physicalSensor.getValue() == null) {
 				isError = true;
+			}
+			if(inputCurveEnabled.isSelected()){
+				if(data.inputCurve==null){
+					isError = true;
+				}
 			}
 			if (inputGearingRatio.getText() == null) {
 				isError = true;
@@ -92,7 +258,7 @@ public class NumericSensorController implements Initializable {
 			if (physicalRate.getValue() == null) {
 				isError = true;
 			}
-			if (rel.getText() == null) {
+			if (rel.getValue() == null) {
 				isError = true;
 			}
 			if (physicalAux.getValue() == null) {
@@ -113,7 +279,60 @@ public class NumericSensorController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-	//TODO: add device save configuration on new value listners
+
+		// input curve enabled listener. This changes the error icon, and the enabled property of the buttons, nothing else.
+		inputCurveEnabled.selectedProperty().addListener((options, oldValue, newValue) -> {
+			if(inputCurveEnabled.isSelected()){
+				selectCurve.setDisable(false);
+				view.setDisable(false);
+			}else{
+				selectCurve.setDisable(true);
+				view.setDisable(true);
+			}
+			updateIcons();
+		});
+
+		// select curve button on click listener
+		selectCurve.setOnAction(new EventHandler<ActionEvent>(){
+			@Override public void handle(ActionEvent e) {
+				FileChooser fileChooser = new FileChooser();
+				fileChooser.setTitle("Open Resource File");
+				File datafile = fileChooser.showOpenDialog(selectCurve.getScene().getWindow());
+				data.inputCurve = datafile;
+				loadPointsFromCsvFile(datafile);
+				System.out.println(data.inputCurve.toString());
+				updateIcons();
+			}
+		});
+
+		// select curve button on click listener
+		view.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("responseCurveDialog.fxml"));
+				Parent dlg = null;
+				try {
+					dlg = fxmlLoader.load();
+				} catch (IOException e) {
+					return;
+				}
+				responseCurveViewController viewController = fxmlLoader.<responseCurveViewController>getController();
+
+				// set the datapoints
+				viewController.setDataPoints(data.inputCurvePoints);
+
+				// show the graph in a modal dialog box
+				Scene scene = new Scene(dlg, 800, 600);
+				Stage stage = new Stage();
+				stage.initModality(Modality.APPLICATION_MODAL);
+				stage.setTitle("Sensor Response Curve");
+				stage.setScene(scene);
+				stage.showAndWait();
+			}
+		});
+
+
+		//TODO: add device save configuration on new value listners
 	}
 
 	public void update(Device device, TreeItem<MainScreenController.TreeData> selectedNode){
@@ -150,11 +369,28 @@ public class NumericSensorController implements Initializable {
 
 		// if sensor is non-null, set value according to json. Else, allow for population.
 		if(device.getBindingValueFromKey(selectedNode.getValue().name,"sensor") != null){
-			physicalSensor.setText(device.getBindingValueFromKey(selectedNode.getValue().name,"sensor"));
+			physicalSensor.getItems().clear();
+			physicalSensor.setValue(device.getBindingValueFromKey(selectedNode.getValue().name,"sensor"));
 			physicalSensor.setDisable(true);
 		}else{
 			physicalSensor.setDisable(false);
-			//TODO: add physical sensor select
+			physicalSensor.getItems().clear();
+			data.sensorNames.clear();
+			try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(System.getProperty("user.dir")+"/lib/sensors"))) {
+				for (Path path : stream) {
+					if (!Files.isDirectory(path)) {
+						JsonResultFactory factory = new JsonResultFactory();
+						JsonAbstractValue json = factory.buildFromFile(path);
+						String name = json.getValue("name");
+						data.sensorNames.add(name);
+						physicalSensor.getItems().add(name);
+						//TODO: handle for physical sensor json is held here
+					}
+				}
+			} catch (IOException e) {
+				// unable to find the directory
+			}
+
 		}
 
 		// if input curve is non-null, set value according to json. Else, allow for population.
@@ -162,11 +398,13 @@ public class NumericSensorController implements Initializable {
 			selectCurve.setDisable(true);
 			inputCurveEnabled.setSelected(true);
 			inputCurveEnabled.setDisable(true);
+			selectCurve.setDisable(true);
+			view.setDisable(true);
 		}else{
-			selectCurve.setDisable(false);
 			inputCurveEnabled.setSelected(false);
 			inputCurveEnabled.setDisable(false);
-			//TODO: add input curve selection
+			selectCurve.setDisable(true);
+			view.setDisable(true);
 		}
 
 		// if gearing ratio is non-null, set value according to json. Else, allow for population.
@@ -183,7 +421,8 @@ public class NumericSensorController implements Initializable {
 			physicalBaseUnit.setDisable(true);
 		}else{
 			physicalBaseUnit.setDisable(false);
-			//TODO: add population for physicalBaseUnit cbox
+			physicalBaseUnit.getItems().clear();
+			for (String choice:unitsChoices) physicalBaseUnit.getItems().add(choice);
 		}
 
 		// if physical unit modifier is non-null, set value according to json. Else, allow for population.
@@ -200,15 +439,28 @@ public class NumericSensorController implements Initializable {
 			physicalRate.setDisable(true);
 		}else{
 			physicalRate.setDisable(false);
-			//TODO: add population for physicalRate cbox
+			physicalRate.getItems().clear();
+			for (String choice:rateChoices) physicalRate.getItems().add(choice);
+		}
+
+		// if physical aux is non-null, set value according to json. Else, allow for population.
+		if(device.getBindingValueFromKey(selectedNode.getValue().name,"physicalAuxUnit") != null){
+			physicalAux.setValue(device.getBindingValueFromKey(selectedNode.getValue().name,"physicalAuxUnit"));
+			physicalAux.setDisable(true);
+		}else{
+			physicalAux.setDisable(false);
+			physicalAux.getItems().clear();
+			for (String choice:unitsChoices) physicalAux.getItems().add(choice);
 		}
 
 		// if rel is non-null, set value according to json. Else, allow for population.
 		if(device.getBindingValueFromKey(selectedNode.getValue().name,"physicalAuxUnit") != null){
-			rel.setText(device.getBindingValueFromKey(selectedNode.getValue().name,"physicalAuxUnit"));
+			rel.setValue(device.getBindingValueFromKey(selectedNode.getValue().name,"physicalAuxUnit"));
 			rel.setDisable(true);
 		}else{
 			rel.setDisable(false);
+			rel.getItems().clear();
+			for (String choice:relChoices) rel.getItems().add(choice);
 		}
 
 		// if physical aux unit modifier is non-null, set value according to json. Else, allow for population.
@@ -219,60 +471,20 @@ public class NumericSensorController implements Initializable {
 			physicalAuxUnitModifier.setDisable(false);
 		}
 
+		// if physical Aux Rate is non-null, set value according to json. Else, allow for population.
+		if(device.getBindingValueFromKey(selectedNode.getValue().name,"physicalAuxRate") != null){
+			physicalAuxRate.setValue(device.getBindingValueFromKey(selectedNode.getValue().name,"physicalAuxRate"));
+			physicalAuxRate.setDisable(true);
+		}else{
+			physicalAuxRate.setDisable(false);
+			physicalAuxRate.getItems().clear();
+			for (String choice:rateChoices) physicalAuxRate.getItems().add(choice);
+		}
+
 		this.selectedNode = selectedNode;
 		updated = true;
 
-		//update icons
-		if (boundChannel.getValue() == null) {
-			boundChannelIcon.setVisible(true);
-		}else {
-			boundChannelIcon.setVisible(false);
-		}
-		if (physicalSensor.getText() == null) {
-			physicalSensorIcon.setVisible(true);
-		}else {
-			physicalSensorIcon.setVisible(false);
-		}
-		if (inputGearingRatio.getText() == null) {
-			inputGearingRatioIcon.setVisible(true);
-		}else {
-			inputGearingRatioIcon.setVisible(false);
-		}
-		if (physicalBaseUnit.getValue() == null) {
-			physicalBaseUnitIcon.setVisible(true);
-		}else {
-			physicalBaseUnitIcon.setVisible(false);
-		}
-		if (physicalUnitModifier.getText() == null) {
-			physicalUnitModifierIcon.setVisible(true);
-		}else {
-			physicalUnitModifierIcon.setVisible(false);
-		}
-		if (physicalRate.getValue() == null) {
-			physicalRateIcon.setVisible(true);
-		}else {
-			physicalRateIcon.setVisible(false);
-		}
-		if (rel.getText() == null) {
-			relIcon.setVisible(true);
-		}else {
-			relIcon.setVisible(false);
-		}
-		if (physicalAux.getValue() == null) {
-			physicalAuxIcon.setVisible(true);
-		}else {
-			physicalAuxIcon.setVisible(false);
-		}
-		if (physicalAuxRate.getValue() == null) {
-			physicalAuxRateIcon.setVisible(true);
-		}else {
-			physicalAuxRateIcon.setVisible(false);
-		}
-		if (physicalAuxUnitModifier.getText() == null) {
-			physicalAuxRateModifierIcon.setVisible(true);
-		}else {
-			physicalAuxRateModifierIcon.setVisible(false);
-		}
+		updateIcons();
 	}
 
 
