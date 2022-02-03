05ba826057da7e4ce73e0dd2aa39b2bd78d6c19c
Rephael B. Jackson <rephael_jackson@yahoo.com>
2022-02-02 21:35:04 -0700

    US1Task21: Added unit testing to project and provided example using robot

---
+++ configurator/src/main/java/org/picmg/unitTest/AppTest.java
@@ -0,0 +1,14 @@
+package org.picmg.unitTest;
+
+import javafx.application.Application;
+import org.junit.Test;
+import org.picmg.configurator.TestApp;
+
+public class AppTest {
+
+
+    @Test
+    public void JavaFXTest() {
+        Application.launch(TestApp.class);
+    }
+}
