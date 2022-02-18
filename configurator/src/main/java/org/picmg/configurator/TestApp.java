2f58bb81306f6987e7a90300a077cf9ec15599b1
GianniConsiglio <59891516+GianniConsiglio@users.noreply.github.com>
2022-02-17 21:14:14 -0700

    fixed test issue

--- configurator/src/main/java/org/picmg/configurator/TestApp.java
+++ configurator/src/main/java/org/picmg/configurator/TestApp.java
@@ -38,7 +38,6 @@ public class TestApp extends App{
             stage.setTitle("PICMG Configurator");
             stage.setScene(scene);
             stage.show();
-            clickReset(scene);
             System.out.println("Here is where the test would go");
         } catch (IOException e) {
             System.out.println(e);
@@ -46,50 +45,9 @@ public class TestApp extends App{
     }
 
     public void clicks(Scene scene) throws InterruptedException {
+        RobotUtils.clickReset(scene);
         RobotUtils.clickSensors(scene);
         RobotUtils.clickEffecters(scene);
         RobotUtils.close();
-    }
-    public void clickSensors(Scene scene)
-    {
-        Window area = scene.getWindow();
-        Node sensorTab = scene.lookup("#sensorTab");
-        Robot robot = new Robot();
-        Point2D point = sensorTab.localToScene(0,0);
-        robot.mouseMove(point.getX()+scene.getX()+area.getX(),point.getY()+scene.getY()+area.getY());
-        robot.mousePress(MouseButton.PRIMARY);
-        robot.mouseRelease(MouseButton.PRIMARY);
-        System.out.println("Done With testing");
-    }
-    public void clickReset(Scene scene) {
-        try {
-
-            Window area = scene.getWindow();
-            Node resetButton = scene.lookup("#resetMenu");
-            Robot robot = new Robot();
-            Point2D point = resetButton.localToScene(0, 0);
-            robot.mouseMove(point.getX() + scene.getX() + area.getX()+10, point.getY() + scene.getY() + area.getY()+10);
-            robot.mouseClick(MouseButton.PRIMARY);
-            Thread thread = new Thread(()->{
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Platform.runLater(()->{
-                    List<Window> temp = Stage.getWindows().stream().collect(Collectors.toList());
-                    Window resetOk = temp.get(1);
-                    Node ok = resetOk.getScene().lookup("#resetOk");
-                    Point2D okArea = ok.localToScene(0,0);robot.mouseMove((double)(okArea.getX() + resetOk.getX() + resetOk.getScene().getX()), (double)( okArea.getY()+ resetOk.getY() + resetOk.getScene().getY()));
-                    robot.mousePress(MouseButton.PRIMARY);
-                    robot.mouseRelease(MouseButton.PRIMARY);
-
-                });
-
-            });
-            thread.start();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
     }
 }
