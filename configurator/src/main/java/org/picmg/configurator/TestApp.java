8d924c7220ce432be59afd5c940ad21afa79e4bb
GianniConsiglio <59891516+GianniConsiglio@users.noreply.github.com>
2022-02-17 21:18:09 -0700

    removed unused imports

--- configurator/src/main/java/org/picmg/configurator/TestApp.java
+++ configurator/src/main/java/org/picmg/configurator/TestApp.java
@@ -1,31 +1,13 @@
 package org.picmg.configurator;
 
-import javafx.application.Platform;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.ReadOnlyObjectProperty;
-import javafx.collections.ObservableList;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.geometry.Point2D;
-import javafx.scene.Node;
-import javafx.scene.control.TreeView;
-import javafx.geometry.Bounds;
-import javafx.geometry.Point2D;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseButton;
-import javafx.scene.robot.Robot;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import org.picmg.test.unitTest.RobotUtils;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class TestApp extends App{
 
