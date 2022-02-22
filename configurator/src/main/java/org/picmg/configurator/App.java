870a3b8bbabca11356bd3ea44f46f54aefb739f2
GianniConsiglio <59891516+GianniConsiglio@users.noreply.github.com>
2022-02-22 08:56:37 -0700

    US#64Task#80 added a image to the application window

--- configurator/src/main/java/org/picmg/configurator/App.java
+++ configurator/src/main/java/org/picmg/configurator/App.java
@@ -30,6 +30,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -94,7 +95,7 @@ public class App extends Application {
 	    try {
 			root = FXMLLoader.load(getClass().getClassLoader().getResource("topTabScene.fxml"));
 			Scene scene = new Scene(root, 1024, 768);
-
+			stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("picmg_logo.png")));
 			stage.setTitle("PICMG Configurator");
 			stage.setScene(scene);
 			stage.show();
