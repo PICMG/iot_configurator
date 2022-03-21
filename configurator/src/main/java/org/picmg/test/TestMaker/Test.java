46bfce2622f076305e9ee0ab9b525fd7d9e9d500
Rephael B. Jackson <rephael_jackson@yahoo.com>
2022-03-21 15:04:18 -0700

    TestWriter now generates a file

--- configurator/src/main/java/org/picmg/test/TestMaker/Test.java
+++ configurator/src/main/java/org/picmg/test/TestMaker/Test.java
@@ -7,7 +7,7 @@ public class Test {
     private String name;
     private ArrayList<Step> steps;
 
-    private class Step
+    public class Step
     {
         public String id;
         public String data;
@@ -32,20 +32,48 @@ public class Test {
         steps = new ArrayList<Step>();
     }
 
+    /**
+     * This sets the test name
+     * @param name
+     */
     public void setName(String name)
     {
         this.name = name;
     }
+
+    /**
+     * This method gets the test name
+     * @return
+     */
     public String getName()
     {
         return name;
     }
+
+    /**
+     * This method adds a step to the test
+     * @param type
+     * @param id
+     * @param data
+     */
     public void addStep(String type, String id, String data)
     {
         Step newStep = new Step(type,id,data);
         steps.add(newStep);
     }
 
+    /**
+     * This method returns the steps for the test
+     * @return
+     */
+    public ArrayList<Step> getSteps()
+    {
+        return steps;
+    }
+
+    /**
+     * This method is for debugging
+     */
     public void print()
     {
         System.out.println("Test Name is " + name);
--- configurator/src/main/java/org/picmg/test/TestMaker/TestWriter.java
+++ configurator/src/main/java/org/picmg/test/TestMaker/TestWriter.java
@@ -1,9 +1,14 @@
 package org.picmg.test.TestMaker;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TestWriter {
 
 
     private static TestWriter writer;
+    private BufferedWriter outputWriter;
     private TestContainer currentTestContainer;
     private TestWriter()
     {
@@ -14,29 +19,32 @@ public class TestWriter {
      * @param test
      */
 
-    public void createTest(TestContainer test)
-    {
+    public void createTest(TestContainer test) throws IOException {
         currentTestContainer = test;
+        String directory = System.getProperty("user.dir")+"/src/test/org/picmg/test/integrationTest/" + currentTestContainer.getTestContainerName() + ".java";
+        createWriter(directory);
         writeHeader();
         writeImports();
         writeClass();
         currentTestContainer =  null;
     }
-    private void writeHeader()
-    {
-        System.out.print("/**\n\tThis is a generated integration test using the testMaker application\n*/\n");
+
+    private void createWriter(String path) throws IOException {
+        outputWriter = new BufferedWriter(new FileWriter(path));
+    }
+    private void writeHeader() throws IOException {
+        outputWriter.write("package org.picmg.test.integrationTest;\n/**\n\tThis is a generated integration test using the testMaker application\n*/\n");
     }
 
-    private void writeClass()
-    {
-        System.out.println("public class " + currentTestContainer.getTestContainerName() + " extends Application\n{\n");
-        // Write test calls
+    private void writeClass() throws IOException {
+        outputWriter.write("public class " + currentTestContainer.getTestContainerName() + " extends Application\n{\n");
+        writeRobotMethods();
         writeTests();
-        System.out.println("}");
+        outputWriter.write("}");
+        outputWriter.close();
     }
 
-    private void writeTests()
-    {
+    private void writeTests() throws IOException {
         writeStart();
         for(Test t : currentTestContainer.getTest())
         {
@@ -44,31 +52,24 @@ public class TestWriter {
         }
     }
 
-     private void writeStart()
-     {
-         System.out.print("\t@Override\n\tpublic void start(Stage stage)\n\t{\n\t\tParent root\n\t\ttry \n\t\t{\n\t\t\troot =FXMLLoader.load(getClass().getClassLoader().getResource(\"" +
+     private void writeStart() throws IOException {
+         outputWriter.write("\t@Override\n\tpublic void start(Stage stage)\n\t{\n\t\tParent root;\n\t\ttry \n\t\t{\n\t\t\troot =FXMLLoader.load(getClass().getClassLoader().getResource(\"" +
                   currentTestContainer.getFileToLoad() +"\"));\n");
 
-         System.out.print("\t\t\tScene scene = new Scene(root, 1024, 768);\n\t\t\tstage.setTitle(\"PICMG Configurator\");\n\t\t\tstage.setScene(scene);\n\t\t\tstage.show();\n\t\t\trobotCalls(stage);");
-         System.out.println("\t\t\n\t\t}\n\t\tcatch (IOException e) {\n\t\t\tSystem.out.println(e);\n\t\t}\n\t}");
+         outputWriter.write("\t\t\tScene scene = new Scene(root, 1024, 768);\n\t\t\tstage.setTitle(\"PICMG Configurator\");\n\t\t\tstage.setScene(scene);\n\t\t\tstage.show();\n\t\t\trobotCalls();\n");
+         outputWriter.write("\t\t\tRobotUtils.setStage(stage);\n");
+         outputWriter.write("\t\t\n\t\t}\n\t\tcatch (IOException e) {\n\t\t\tSystem.out.println(e);\n\t\t}\n\t}\n");
 
-         /*          public void start(Stage stage) {
-              Parent root;
-              try {
-                  root = FXMLLoader.load(getClass().getClassLoader().getResource("topTabScene.fxml"));
-                  Scene scene = new Scene(root, 1024, 768);
-                  stage.setTitle("PICMG Configurator");
-                  stage.setScene(scene);
-                 stage.show();
-                  clicks(scene);
-                  System.out.println("Here is where the test would go");
-              } catch (IOException e) {
-                  System.out.println(e);
-             }
-          }*/
      }
-    private void writeImports()
-    {
+     private void writeRobotMethods() throws IOException {
+         outputWriter.write("\tpublic void robotCalls()\n\t{\n");
+        for(Test t : currentTestContainer.getTest())
+        {
+            outputWriter.write("\t\t" + t.getName().replaceAll(" ", "")+"();" + "\n");
+        }
+         outputWriter.write("\t}\n");
+     }
+    private void writeImports() throws IOException {
         String imports = "javafx.fxml.FXMLLoader," +
                 "javafx.scene.Parent," +
                 "javafx.scene.Scene," +
@@ -76,20 +77,41 @@ public class TestWriter {
                 "javafx.application.Application," +
                 "org.junit.Test," +
                 "static org.junit.Assert.*," +
-                "org.picmg.test.unitTest.RobotUtils";
+                "org.picmg.test.integrationTest.RobotUtils," +
+                "java.io.IOException";
 
         String[] importsToWrite = imports.split(",");
         for(String s : importsToWrite)
         {
-            System.out.print("import " + s + ";\n");
+            outputWriter.write("import " + s + ";\n");
         }
     }
 
-    private void writeTest(Test t)
-    {
-        System.out.println("\tpublic void " + t.getName().replaceAll(" ", "_") +"\n\t{\n\n\t}\n");
+    private void writeTest(Test t) throws IOException {
+        outputWriter.write("\tpublic void " + t.getName().replaceAll(" ", "") +"()\n\t{\n");
 
+        for(Test.Step s : t.getSteps())
+        {
+            switch(s.type)
+            {
+                case "Click":
+                    outputWriter.write("\t\tRobotUtils.dummyClick(" + "\"" + s.id + "\"" + ");\n");
+                    break;
+                case "Type":
+                    outputWriter.write("\t\tRobotUtils.dummyType(" + "\"" +s.data + "\"" +");\n");
+                    break;
+                case "Check":
+                    outputWriter.write("\t\tRobotUtils.dummyCheck(" + "\"" +s.id + "\"" + "," + "\"" +s.data + "\"" + ");\n");
+                    break;
+            }
+        }
+        outputWriter.write("\t}\n");
     }
+
+    /**
+     * This method returns an instance of the testWriter
+     * @return
+     */
     public static TestWriter getInstance()
     {
         if(writer == null)
