0362fe6c14fc130675c2828de31eef2131259425
Alex Baratti <alexsmbaratti@gmail.com>
2022-03-31 19:02:08 -0700

    Achieved 70% test coverage for public methods in SensorsTabController

--- configurator/src/test/org/picmg/test/unitTest/SensorTableDataTest.java
+++ configurator/src/test/org/picmg/test/unitTest/SensorTableDataTest.java
@@ -1,8 +1,11 @@
 package org.picmg.test.unitTest;
 
+import javafx.geometry.Point2D;
 import org.junit.Before;
 import org.junit.Test;
 import org.picmg.configurator.SensorsTabController;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -118,5 +121,163 @@ public class SensorTableDataTest {
         assertTrue(sensorTableData.isRate());
         sensorTableData.setRate(false);
         assertFalse(sensorTableData.isRate());
+    }
+
+    @Test
+    public void testIsQuadrature() {
+        assertFalse(sensorTableData.isQuadrature());
+    }
+
+    @Test
+    public void testSetQuadrature() {
+        assertFalse(sensorTableData.isQuadrature());
+        sensorTableData.setQuadrature(true);
+        assertTrue(sensorTableData.isQuadrature());
+        sensorTableData.setQuadrature(false);
+        assertFalse(sensorTableData.isQuadrature());
+    }
+
+    @Test
+    public void testGetMaxSampleRate() {
+        assertNull(sensorTableData.getMaxSampleRate());
+    }
+
+    @Test
+    public void testSetMaxSampleRate() {
+        assertNull(sensorTableData.getMaxSampleRate());
+        sensorTableData.setMaxSampleRate("5.0");
+        assertEquals("5.0", sensorTableData.getMaxSampleRate());
+    }
+
+    @Test
+    public void testGetBaseUnit() {
+        assertNull(sensorTableData.getBaseUnit());
+    }
+
+    @Test
+    public void testSetBaseUnit() {
+        assertNull(sensorTableData.getBaseUnit());
+        sensorTableData.setBaseUnit("Ounces");
+        assertEquals("Ounces", sensorTableData.getBaseUnit());
+    }
+
+    @Test
+    public void testGetUnitModifier() {
+        assertNull(sensorTableData.getUnitModifier());
+    }
+
+    @Test
+    public void testSetUnitModifier() {
+        assertNull(sensorTableData.getUnitModifier());
+        sensorTableData.setUnitModifier("10");
+        assertEquals("10", sensorTableData.getUnitModifier());
+    }
+
+    @Test
+    public void testGetRateUnit() {
+        assertNull(sensorTableData.getRateUnit());
+    }
+
+    @Test
+    public void testSetRateUnit() {
+        assertNull(sensorTableData.getRateUnit());
+        sensorTableData.setRateUnit("Ounces");
+        assertEquals("Ounces", sensorTableData.getRateUnit());
+    }
+
+    @Test
+    public void testGetAuxUnit() {
+        assertNull(sensorTableData.getAuxUnit());
+    }
+
+    @Test
+    public void testSetAuxUnit() {
+        assertNull(sensorTableData.getAuxUnit());
+        sensorTableData.setAuxUnit("Ounces");
+        assertEquals("Ounces", sensorTableData.getAuxUnit());
+    }
+
+    @Test
+    public void testGetAuxModifier() {
+        assertNull(sensorTableData.getAuxModifier());
+    }
+
+    @Test
+    public void testSetAuxModifier() {
+        assertNull(sensorTableData.getAuxModifier());
+        sensorTableData.setAuxModifier("10");
+        assertEquals("10", sensorTableData.getAuxModifier());
+    }
+
+    @Test
+    public void testGetRel() {
+        assertNull(sensorTableData.getRel());
+    }
+
+    @Test
+    public void testSetRel() {
+        assertNull(sensorTableData.getRel());
+        sensorTableData.setRel("10");
+        assertEquals("10", sensorTableData.getRel());
+    }
+
+    @Test
+    public void testGetAuxRateUnit() {
+        assertNull(sensorTableData.getAuxRateUnit());
+    }
+
+    @Test
+    public void testSetAuxRateUnit() {
+        assertNull(sensorTableData.getAuxRateUnit());
+        sensorTableData.setAuxRateUnit("10");
+        assertEquals("10", sensorTableData.getAuxRateUnit());
+    }
+
+    @Test
+    public void testGetPlusAccuracy() {
+        assertNull(sensorTableData.getPlusAccuracy());
+    }
+
+    @Test
+    public void testSetPlusAccuracy() {
+        assertNull(sensorTableData.getPlusAccuracy());
+        sensorTableData.setPlusAccuracy("1");
+        assertEquals("1", sensorTableData.getPlusAccuracy());
+    }
+
+    @Test
+    public void testGetMinusAccuracy() {
+        assertNull(sensorTableData.getMinusAccuracy());
+    }
+
+    @Test
+    public void testSetMinusAccuracy() {
+        assertNull(sensorTableData.getMinusAccuracy());
+        sensorTableData.setMinusAccuracy("1");
+        assertEquals("1", sensorTableData.getMinusAccuracy());
+    }
+
+    @Test
+    public void testGetOutputUnits() {
+        assertNull(sensorTableData.getOutputUnits());
+    }
+
+    @Test
+    public void testSetOutputUnits() {
+        assertNull(sensorTableData.getOutputUnits());
+        sensorTableData.setOutputUnits("Ounces");
+        assertEquals("Ounces", sensorTableData.getOutputUnits());
+    }
+
+    @Test
+    public void testGetOutputCurve() {
+        ArrayList<Point2D> emptyOutputCurve = new ArrayList<Point2D>();
+        assertEquals(emptyOutputCurve, sensorTableData.getOutputCurve());
+        assertTrue(sensorTableData.getOutputCurve().isEmpty());
+    }
+
+    @Test
+    public void testGetSavePath() {
+        assertNull(sensorTableData.getSavePath());
     }
 }
