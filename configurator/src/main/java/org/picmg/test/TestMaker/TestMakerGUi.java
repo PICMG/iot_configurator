b69ebb1c6270243dee62959c08a347bf0749d859
Rephael B. Jackson <rephael_jackson@yahoo.com>
2022-04-07 19:44:44 -0700

    Added more buttons and functions

--- configurator/src/main/java/org/picmg/test/TestMaker/TestMakerGUi.java
+++ configurator/src/main/java/org/picmg/test/TestMaker/TestMakerGUi.java
@@ -38,11 +38,18 @@ public class TestMakerGUi extends Application {
     @FXML private TextField stringInputField;
     @FXML private Button addB;
     @FXML private Button removeB;
+    @FXML private Button clearB;
+    @FXML private Button addB2;
+    @FXML private Button removeB2;
+    @FXML private Button clearB2;
     @FXML private Button resetB;
 
     // Right components
     @FXML private TextField nameField;
-    @FXML private ListView outputView;
+
+    // List views
+    @FXML private ListView stepView;
+    @FXML private ListView testView;
 
 
     @Override
@@ -56,18 +63,9 @@ public class TestMakerGUi extends Application {
             primaryStage.show();
 
             loadData(scene);
+            actionListeners();
 
 
-
-
-            // Action listeners
-            resetB.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    idField.setText("");
-                    stringInputField.setText("");
-                }
-            });
         }
         catch(Exception e)
         {
@@ -77,17 +75,143 @@ public class TestMakerGUi extends Application {
     }
 
 
+    private void actionListeners()
+    {
+        // Action listeners
+        resetB.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                reset();
+            }
+        });
+        addB.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                addStep();
+            }
+        });
+
+        removeB.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                removeItem();
+            }
+        });
+    }
     public void loadData(Scene scene) throws IOException {
 
 
         //TextFields
         stringInputField =(TextField) scene.lookup("#stringInputField");
         idField =(TextField) scene.lookup("#idField");
+
         // Assign the buttons
         addB = (Button) scene.lookup("#addB");
         removeB = (Button) scene.lookup("#removeB");
+        clearB = (Button) scene.lookup("clearB");
+
         resetB = (Button) scene.lookup("#resetB");
 
+        // Assign radio buttons
+        typeB = (RadioButton) scene.lookup("#typeRadio");
+        clickB = (RadioButton) scene.lookup("#clickRadio");
+        testB = (RadioButton) scene.lookup("#testRadio");
+
+        //
+        stepView = (ListView)  scene.lookup("#stepView");
+        testView = (ListView)  scene.lookup("#testView");
+
+
+
+
+    }
+
+
+    private void reset()
+    {
+        idField.setText("");
+        stringInputField.setText("");
+        typeB.setSelected(true);
+    }
+
+    private void removeItem()
+    {
+        if(stepView.getSelectionModel().getSelectedIndex() != -1)
+        {
+            System.out.println("I have something");
+        }
+        else
+        {
+            System.out.println("I Nothing");
+        }
+    }
+
+    private void addStep()
+    {
+        String output ="";
+        if(typeB.isSelected())
+        {
+            if(stringInputField.getText().equals(""))
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Empty Input");
+                alert.setContentText("You have entered in an empty string as the input");
+                alert.showAndWait();
+                return;
+            }
+            output += "Type ";
+            output += stringInputField.getText() + " ";
+        }
+        else if(clickB.isSelected())
+        {
+            if(idField.getText().equals(""))
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Missing ID");
+                alert.setContentText("An ID is required");
+                alert.showAndWait();
+                return;
+            }
+            output += "Click ";
+            output += idField.getText() + " ";
+        }
+        else if(testB.isSelected())
+        {
+            boolean error = false;
+            String content = "";
+            if(stringInputField.getText().equals(""))
+            {
+                System.out.println("GER");
+
+                error = true;
+                content += "You have entered in an empty string as the input\n";
+            }
+            if(idField.getText().equals(""))
+            {
+                System.out.println("GER");
+
+                error = true;
+                content +="An ID is required";
+            }
+            if(error)
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Missing input");
+                alert.setContentText(content);
+                alert.showAndWait();
+                return;
+
+            }
+            output += "Test ";
+            output += idField.getText() + " ";
+            output += stringInputField.getText() + " ";
+
+        }
+        stepView.getItems().add(output);
+        stepView.setItems(stepView.getItems());
+
+        testView.getItems().add(output);
+        testView.setItems(testView.getItems());
 
     }
     public static void main(String[] args) {
