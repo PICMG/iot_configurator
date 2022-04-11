87b441029df24b91bdeb6677ce3ee50a359642f8
Alex Baratti <alexsmbaratti@gmail.com>
2022-04-11 11:58:29 -0700

    Adds JSON capabilities, additional fields, and tests for StateSet

--- configurator/src/main/java/org/picmg/configurator/StateSet.java
+++ configurator/src/main/java/org/picmg/configurator/StateSet.java
@@ -1,24 +1,34 @@
 package org.picmg.configurator;
 
+import org.picmg.jsonreader.JsonArray;
+import org.picmg.jsonreader.JsonObject;
+import org.picmg.jsonreader.JsonValue;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class StateSet {
 
     private String name;
+    private int id;
     private String vendorName;
+    private int vendorIANA;
     private Map<String, String> statesList; // Pairs between names and keys
 
     public StateSet(String name, String vendorName) {
-        this.name = name;
-        this.vendorName = vendorName;
-        this.statesList = new HashMap<>();
+        this(name, vendorName, new HashMap<>());
     }
 
     public StateSet(String name, String vendorName, Map<String, String> statesList) {
+        this(name, -1, vendorName, -1, statesList);
+    }
+
+    public StateSet(String name, int id, String vendorName, int vendorIANA, Map<String, String> statesList) {
         this.name = name;
         this.vendorName = vendorName;
         this.statesList = statesList;
+        this.id = id;
+        this.vendorIANA = vendorIANA;
     }
 
     public String getName() {
@@ -51,5 +61,16 @@ public class StateSet {
 
     public void setState(String key, String value) {
         statesList.put(key, value);
+    }
+
+    public JsonObject toJSON() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.put("name", new JsonValue(name));
+        jsonObject.put("vendorName", new JsonValue(vendorName));
+        jsonObject.put("vendorIANA", new JsonValue(String.valueOf(vendorIANA)));
+        jsonObject.put("stateSetID", new JsonValue(String.valueOf(id)));
+        JsonArray oemStateValueRecords = new JsonArray();
+        jsonObject.put("oemStateValueRecords", oemStateValueRecords);
+        return jsonObject;
     }
 }
